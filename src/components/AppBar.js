69a75d02edfe78541c3f2a6039bc43bade5e7458
InKyungWoo <chelsea1201@naver.com>
2024-03-02 00:41:51 +0900

    :sparkles: feat: AppBar 컴포넌트 구현

---
+++ src/components/AppBar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const AppBar = () => {
+    // 로고 클릭시 홈화면으로 이동
+    const navigate = useNavigate();
+    const goToHome = () => {
+        navigate("/");
+    };
+
+    return (
+        <Container>
+            <Logo 
+                src="/images/devprep.png"
+                onClick={goToHome}
+            />
+        </Container>
+    );
+};
+
+export default AppBar;
+
+const Container = styled.div`
+    width: 24.375rem;
+    height: 4.375rem;
+    display: flex; 
+    justify-content: center; /* 수평 가운데 정렬 */
+    align-items: center; /* 수직 가운데 정렬 */
+    background: #E3F3FB;
+`;
+
+const Logo = styled.img`
+    width: 9.875rem;
+    height: 2.4375rem;
+    flex-shrink: 0;
+`;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 import symbol from "../assets/images/symbol.png"
+import AppBar from "../components/AppBar";
 
 const Home = () => {
     return (
       <Container>
+        <AppBar />
         <LogoImg src={symbol} />
       </Container>
     );
@@ -24,6 +26,6 @@ const Home = () => {
   width: 12.5rem;
   height: 12.40625rem;
   flex-shrink: 0;
-  margin-left: 4rem;
-  margin-top: 8.22rem;
+  margin-left: 4.44rem;
+  margin-top: 4.94rem;
 `
