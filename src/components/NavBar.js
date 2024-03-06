5ed14d1e1506e743204331e76c8b191801caf62a
InKyungWoo <chelsea1201@naver.com>
2024-03-07 03:29:04 +0900

    :sparkles: feat: NavBar icon hover, click event 구현

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const NavBar = () => {
-    const icons = ["home", "game", "community", "mypage"];
+    const icons = [
+        { name: "home", src: "/images/icons/home.svg" },
+        { name: "game", src: "/images/icons/game.svg" },
+        { name: "community", src: "/images/icons/community.svg" },
+        { name: "mypage", src: "/images/icons/mypage.svg" }
+    ];
+
+    const [selectedIcon, setSelectedIcon] = useState(null);
+    const handleIconClick = (index) => {
+        setSelectedIcon(index);
+    };
 
     return (
         <Container>
-            {icons.map((name, index) => (
-                <IconWrapper key={index}>
-                    <Icon src={`/images/icons/${name}.svg`} />
+            {icons.map((icon, index) => (
+                <IconWrapper 
+                    key={index} 
+                    onClick={() => handleIconClick(index)} 
+                    selected={selectedIcon === index} 
+                >
+                    <Icon 
+                        src={selectedIcon === index ? `images/icons/${icon.name}_selected.svg` : icon.src} />
                 </IconWrapper>
             ))}
         </Container>
@@ -30,20 +45,23 @@ const Container = styled.div`
     // 하단 고정 추가 속성
     position: absolute;
     bottom: 0;
-`
+`;
 
 const IconWrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
     flex-grow: 1;
-    //text-align: center;
-`
+    
+    &:hover {
+        filter: invert(100%);
+    }
+`;
+
 const Icon = styled.img`
-    //${props => !props.last && "margin-right: 3.5rem;"}
     margin-right: 3.5rem;
     
     &:last-child {
         margin-right: 0; // 마지막 아이콘은 마진 제거
     }
-`
+`;
