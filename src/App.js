6ae7e7fb03f2895d3166140bfa2b8f35b3abe9a8
InKyungWoo <chelsea1201@naver.com>
2024-03-10 03:30:11 +0900

    :truck: move: 그룹화 폴더 route 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import { Route, Routes } from "react-router-dom";
 
 // pages
-import Home from "./pages/Home";
-import MyPage from "./pages/MyPage/MyPage";
+import Home from "./pages/home";
+import Prep from './pages/prep';
+import MyPage from "./pages/mypage";
+import Game from './pages/game';
+import Community from './pages/community';
+
 
 function App() {
     return (
         <div>
             <Routes>
+                {/* 홈 화면 */}
                 <Route index element={<Home />} />
+
+                {/* 면접 연습 페이지 */}
+                <Route path="/prep" element={<Prep />} />
+
+                {/* 마이페이지 */}
                 <Route path="/mypage" element={<MyPage />} />
+
+                {/* 게임 페이지 */}
+                <Route path="/game" element={<Game />} />
+
+                {/* 커뮤니티 페이지 */}
+                <Route path="/community" element={<Community />} />
+
             </Routes>
         </div>
     );
