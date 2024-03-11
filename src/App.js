3ba6e1fe5c5946bc06bcb49c44b2d55d34e5ac9d
InKyungWoo <chelsea1201@naver.com>
2024-03-11 19:26:21 +0900

    :truck: move: 게임 솔루션 페이지 이름 변경

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import { Route, Routes } from "react-router-dom";
 
 // pages
 import Home from "./pages/home";
-import Prep from './pages/prep';
+import Prep from "./pages/prep";
 
 import MyPage from "./pages/mypage";
 
-import Game from './pages/game';
+import Game from "./pages/game";
 import Quiz from "./pages/game/Quiz";
-import Solution from "./pages/game/Solution";
+import Answer from "./pages/game/Answer";
 
-import Community from './pages/community';
+import Community from "./pages/community";
 
 
 function App() {
@@ -29,7 +29,7 @@ function App() {
                 {/* 게임 페이지 */}
                 <Route path="/game" element={<Game />} />
                 <Route path="/game/quiz" element={<Quiz />} />
-                <Route path="/game/solution" element={<Solution/>} />
+                <Route path="/game/answer" element={<Answer/>} />
 
                 {/* 커뮤니티 페이지 */}
                 <Route path="/community" element={<Community />} />
